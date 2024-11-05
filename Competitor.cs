ef97301af6175e303a10b6b8e10650b8ce25bc42
Ny19MarcalekM <matemarcalek@gmail.com>
2024-11-05 10:06:15 +0100

    Add project files.

---
+++ Competitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triatlon;
+
+internal class Competitor
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+    public string RaceNumber { get; set; }
+    public bool Gender { get; set; }
+    public string AgeCategory { get; set; }
+    public Dictionary<string, TimeSpan> RaceTimes { get; set; }
+    public double TotalTimeInSec => RaceTimes.Values.Sum(x => x.TotalSeconds);
+
+    public Competitor(string row)
+    {
+        var v = row.Split(';');
+        Name = v[0];
+        YearOfBirth = int.Parse(v[1]);
+        RaceNumber = v[2];
+        Gender = v[3] == "f";
+        AgeCategory = v[4];
+        RaceTimes = new()
+        {
+            {"swimming", TimeSpan.Parse(v[5])},
+            {"1st depot", TimeSpan.Parse(v[6])},
+            {"cycling", TimeSpan.Parse(v[7])},
+            {"2nd depot", TimeSpan.Parse(v[8])},
+            {"running", TimeSpan.Parse(v[9])}
+        };
+    }
+
+    public override string ToString()
+    {
+        return $"\t[{RaceNumber}] {Name} {AgeCategory} ({(Gender ? "férfi" : "nő")})\n";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using Triatlon;
+
+List <Competitor> competitors = new();
+using StreamReader sr = new(
+    path: @"../../../src/forras.txt",
+    encoding: System.Text.Encoding.UTF8);
+while(!sr.EndOfStream)competitors.Add(new(sr.ReadLine()));
+Console.WriteLine($"Célba érkezők száma: {competitors.Count}");
+
+//hány %-a férfi a versenyzőknek?
+var f1 = competitors.Where(c => c.Gender).ToList().Count / (float)competitors.Count * 100;
+Console.WriteLine(f1 + "%-a férfi a versenyzőknek\n");
+
+//első célbaérkező női versenyző
+var f2 = competitors.Where(c => !c.Gender).MinBy(c => c.TotalTimeInSec);
+Console.WriteLine($"Az első célba érkező nő: {f2.ToString()}\n");
+
+//átlagos depóban töltött idő
+var f3 = competitors.Average(c  => (c.RaceTimes["1st depot"] + c.RaceTimes["2nd depot"]).TotalSeconds / 2);
+Console.WriteLine($"Átlagos depóban töltött idő: {f3:0.00} másodperc\n");
+
+//korkategóriánként a versenyzők száma
+var f4 = competitors.GroupBy(c => c.AgeCategory).OrderBy(c => c.Key);
+Console.WriteLine("Versenyzők száma kategóriánként:");
+foreach(var f in f4) Console.WriteLine($"\t{f.Key,11}: {f.Count(),2} fő");
+
+//átlagos versenyidő férfiaknél korkategóriánként
+var f5 = competitors
+    .Where(c => c.Gender)
+    .GroupBy(c => c.AgeCategory)
+    .ToDictionary(g => g.Key, g => g.Average(c => c.TotalTimeInSec))
+    .OrderBy(d =>d.Value);
+Console.WriteLine($"Átlagos versenyidő a férfiaknál korkategóriánként:");
+foreach(var f in f5) Console.WriteLine($"\t{f.Key,11}: {f.Value / 60:0.0000} perc");
